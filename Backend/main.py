61fd05274573850d18cb5cb5776465037cf4cf32
Sergi Coca <adaw24scoca@inslaferreria.cat>
2026-01-13 16:53:03 +0100

    models conexio BD

--- Backend/main.py
+++ Backend/main.py
@@ -1,1 +1,11 @@
-print("Hello World")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"mensaje": "Hola alumnos!"}
+
+@app.get("/saludo/{nombre}")
+def read_item(nombre: str):
+    return {"saludo": f"Hola {nombre}!"}
