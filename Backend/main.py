70e6e7fba353fc6701e3fbfec4ab60ab186938e4
ilinaresDAW <adaw24ilinares@inslaferrreria.cat>
2026-01-22 16:52:37 +0100

    Add user and trip endpoints for profile management and trip operations

--- Backend/main.py
+++ Backend/main.py
@@ -19,3 +19,49 @@ def read_root():
 @app.get("/saludo/{nombre}")
 def read_item(nombre: str):
     return {"saludo": f"Hola {nombre}!"}
+
+#USUARIS
+
+@app.get("/users/me")
+def perfilUsuari():
+    return()
+
+@app.put("/users/me/{nom}/{bio}")
+def actualizarPerfil(nom: str, bio: str)
+    return()
+
+
+#VIATGERS
+
+@app.get("/trips")
+def listarViajesDisponibles()
+    return()
+
+@app.get("/trips/{id}")
+def verDetallesID()
+    return()
+
+@app.post("/trips/enroll/{id}")
+def insribirID()
+    return()
+
+@app.post("/trips/leave/{id}")
+def desinsribirID()
+    return()
+
+@app.post("/promote-request")
+def crearPeticio()
+    return()
+
+#CREADORS DE VIATGES
+@app.post("/creator/trips")
+def crearViaje()
+    return()
+
+@app.put("/creator/trips/{id}")
+def editarViaje()
+    return()
+
+@app.delete("/creator/trips/{id}")
+def eliminarViaje()
+    return()
